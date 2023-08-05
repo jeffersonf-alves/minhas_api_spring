23e1f3ffb7bcb15a9771e55d08927d8d1b6d841f
Jefferson Alves <jefff9910@outlook.com>
2023-08-04 23:40:32 -0300

    Update api carros

--- carros/src/main/java/com/example/carros/Api/CarrosController.java
+++ carros/src/main/java/com/example/carros/Api/CarrosController.java
@@ -3,10 +3,7 @@ package com.example.carros.Api;
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -27,5 +24,19 @@ public class CarrosController {
         return service.getCarroById(id);
     }
 
+    @GetMapping("/tipo/{tipo}")
+    public Iterable<Carro> getCarrosByTipo(@PathVariable("tipo") String tipo) {
+        return service.getCarroByTipo(tipo);
+    }
+    @PostMapping
+    public String post(@RequestBody Carro carro) {
+        Carro c = service.save(carro);
 
+        return "Carro salvo com sucesso: "+c.getId();
+    }
+    @PutMapping("/{id}")
+    public String put(@PathVariable("id") Long id, @RequestBody Carro carro) {
+        Carro c = service.update(carro, id);
+        return "Carro alterado com sucesso: "+c.getId();
+    }
 }
--- carros/src/main/java/com/example/carros/domain/CarroService.java
+++ carros/src/main/java/com/example/carros/domain/CarroService.java
@@ -2,6 +2,7 @@ package com.example.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
@@ -17,6 +18,14 @@ public class CarroService {
     public Iterable<Carro> getCarros() {
         return rep.findAll();
     }
+    public Optional<Carro> getCarroById(Long id) {
+        return rep.findById(id);
+    }
+
+    public Iterable<Carro> getCarroByTipo(String tipo) {
+        return rep.findByTipo(tipo);
+    }
+
 
     public List<Carro> getCarrosFake() {
         List<Carro> carros = new ArrayList<>();
@@ -28,7 +37,25 @@ public class CarroService {
         return carros;
     }
 
-    public Optional<Carro> getCarroById(Long id) {
-        return rep.findById(id);
+    public Carro save(Carro carro) {
+        return rep.save(carro);
+    }
+
+    public Carro update(Carro carro, Long id) {
+        Assert.notNull(id, "Não foi possível atualizar o registro!!");
+
+        Optional<Carro> optional = getCarroById(id);
+        if(optional.isPresent()) {
+            Carro db = optional.get();
+
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+            System.out.println("Carro ID: "+db.getId());
+            rep.save(db);
+
+            return db;
+        } else {
+            throw new RuntimeException("Não foi possível atualizar o registro!");
+        }
     }
 }
