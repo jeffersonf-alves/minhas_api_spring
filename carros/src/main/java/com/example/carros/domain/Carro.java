8dd63260299c66bd4609284064878a8c05ea12a1
Jefferson Alves <jefff9910@outlook.com>
2023-08-13 21:25:19 -0300

    Trabalhando com DTO

--- carros/src/main/java/com/example/carros/domain/Carro.java
+++ carros/src/main/java/com/example/carros/domain/Carro.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Data
 public class Carro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,35 +15,35 @@ public class Carro {
     private String nome;
     private String tipo;
 
-    public Carro() {
-    }
-
-    public Carro(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+//    public Carro() {
+//    }
+//
+//    public Carro(Long id, String nome) {
+//        this.id = id;
+//        this.nome = nome;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getTipo() {
+//        return tipo;
+//    }
+//
+//    public void setTipo(String tipo) {
+//        this.tipo = tipo;
+//    }
 }
--- carros/src/main/java/com/example/carros/domain/CarroService.java
+++ carros/src/main/java/com/example/carros/domain/CarroService.java
@@ -27,15 +27,15 @@ public class CarroService {
     }
 
 
-    public List<Carro> getCarrosFake() {
-        List<Carro> carros = new ArrayList<>();
-
-        carros.add(new Carro(1L, "Fusca"));
-        carros.add(new Carro(2L, "Brasilia"));
-        carros.add(new Carro(3L, "Chevette"));
-
-        return carros;
-    }
+//    public List<Carro> getCarrosFake() {
+//        List<Carro> carros = new ArrayList<>();
+//
+//        carros.add(new Carro(1L, "Fusca"));
+//        carros.add(new Carro(2L, "Brasilia"));
+//        carros.add(new Carro(3L, "Chevette"));
+//
+//        return carros;
+//    }
 
     public Carro save(Carro carro) {
         return rep.save(carro);
