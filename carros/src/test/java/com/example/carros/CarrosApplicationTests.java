7ce3bd655f2d0d0d2aef2450c35d3fd62e386af8
Jefferson Alves <jefff9910@outlook.com>
2023-09-04 22:38:22 -0300

    Estudando testes unitarios

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.carros;
 
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
+import com.example.carros.domain.dto.CarroDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
+	@Autowired
+	private CarroService service;
+
+	@Test
+	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("Esportivo");
+
+		service.insert(carro);
+
+//		assertNotNull(c);
+	}
 	@Test
 	void contextLoads() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("Esportivo");
+
+//
+//		assertNotNull(c);
 	}
 
 }
